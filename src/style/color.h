e4778d693010a67642c7dc971003311b45e1081c
K10-K10 <keito.tottori@gmail.com>
2026-06-06 12:31:56 +0000

    feat: new style structure and refactor color enum name

--- src/style/color.h
+++ src/style/color.h
@@ -3,28 +3,38 @@
 namespace terminal::utils {
 // text
 enum class TextColor {
-  text_black = 30,
-  text_red = 31,
-  text_green = 32,
-  text_yellow = 33,
-  text_blue = 34,
-  text_magenta = 35,
-  text_cyan = 36,
-  text_gray = 37
+  Default = 39,
+  Black = 30,
+  Red = 31,
+  Green = 32,
+  Yellow = 33,
+  Blue = 34,
+  Magenta = 35,
+  Cyan = 36,
+  White = 37
 };
 // fill
 enum class FillColor {
-  fill_black = 40,
-  fill_red = 41,
-  fill_green = 42,
-  fill_yellow = 43,
-  fill_blue = 44,
-  fill_magenta = 45,
-  fill_cyan = 46,
-  fill_gray = 47
+  Default = 49,
+  Black = 40,
+  Red = 41,
+  Green = 42,
+  Yellow = 43,
+  Blue = 44,
+  Magenta = 45,
+  Cyan = 46,
+  White = 47
+};
+
+struct Style {
+  TextColor fg = TextColor::Default;
+  FillColor bg = FillColor::Default;
+  bool bold = false;
+  bool inverted = false;
+  bool underline = false;
 };
 
 int printColor(int txt_color, const char* str);
-int printColor(int txt_color, int fill_color, const char* str);
+int printColor(int txt_color, int color, const char* str);
 
 }  // namespace terminal::utils
