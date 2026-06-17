b1dd971f079268fcbf6f2410b3c37c12abe44e40
K10-K10 <keito.tottori@gmail.com>
2026-06-17 15:18:11 +0000

    feat(block): update draw func

--- src/K10-K10/obj/block.cc
+++ src/K10-K10/obj/block.cc
@@ -1,7 +1,9 @@
 #include <K10-K10/core/drawObj.h>
 #include <K10-K10/layout/rect.h>
+#include <K10-K10/line/text.h>
 #include <K10-K10/obj/block.h>
 #include <K10-K10/style/border.h>
+#include <K10-K10/utils/string_helper.h>
 
 namespace krow {
 Block& Block::position(const Rect& r) {
@@ -29,6 +31,38 @@ void Block::draw() {
   for (int x = l; x <= r; ++x) {
     if (has_top) __krow__::drawObj.put(t, x, {s.h, border_style_});
     if (has_bottom) __krow__::drawObj.put(b, x, {s.h, border_style_});
+  }
+
+  if (!title_.empty()) {
+    for (size_t i = 0; i < title_.contents_.size(); ++i) {
+      const auto& row = title_.contents_[i];
+
+      auto draw_alignment = [&](const std::queue<Line>& q,
+                                style::alignment align) {
+        if (q.empty()) return;
+
+        Line line = q.front();
+        int title_len = 0;
+        for (const auto& c : line.contents_) {
+          title_len += __terminal__::get_visual_width(c.first.text_);
+        }
+        int start_ =
+            __terminal__::calc_alignment(align, {l + 1, r - 1}, title_len);
+        for (const auto& l : line.contents_) {
+          auto v_chars = __terminal__::split_by_visual_character(l.first.text_);
+
+          for (const auto& vc : v_chars) {
+            if (start_ > r - 1) break;
+            __terminal__::drawObj.put(t, start_, {vc.c, l.first.style_val});
+            start_ += vc.width;
+          }
+        }
+      };
+
+      draw_alignment(row.left, style::alignment::LEFT);
+      draw_alignment(row.center, style::alignment::CENTER);
+      draw_alignment(row.right, style::alignment::RIGHT);
+    }
   }
 
   for (int y = t; y <= b; ++y) {
