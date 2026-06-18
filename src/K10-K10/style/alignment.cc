660f6ce0a7f1463417009d641b486e01e751d9e9
K10-K10 <keito.tottori@gmail.com>
2026-06-18 13:24:12 +0900

    fix(alignment): calc bug

--- src/K10-K10/style/alignment.cc
+++ src/K10-K10/style/alignment.cc
@@ -6,18 +6,17 @@ namespace __terminal__ {
 int calc_alignment(terminal::style::alignment ali, std::pair<int, int> place,
                    int length) {
   int start_pos = place.first;
-  int end_pos = place.second;
-  int total_width = end_pos - start_pos + 1;
+  int width = place.second;
 
   switch (ali) {
     case terminal::style::alignment::LEFT:
       return start_pos;
 
     case terminal::style::alignment::CENTER:
-      return start_pos + (total_width - length) / 2;
+      return start_pos + (width - length) / 2;
 
     case terminal::style::alignment::RIGHT:
-      return end_pos - length + 1;
+      return width + start_pos - length;
 
     default:
       return start_pos;
