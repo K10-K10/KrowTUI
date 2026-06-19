6ce6394ff401522d04d4e43326225ecdcc5f0217
K10-K10 <keito.tottori@gmail.com>
2026-06-19 05:31:21 +0000

    fix: title rendering

--- src/K10-K10/obj/block.cc
+++ src/K10-K10/obj/block.cc
@@ -6,7 +6,6 @@
 #include <K10-K10/utils/string_helper.h>
 
 namespace krow {
-
 void Block::draw() {
   int l = rect.x;
   int w = (rect.w == FULL) ? __krow__::screen.width() : rect.w;
@@ -15,6 +14,7 @@ void Block::draw() {
   int h = (rect.h == FULL) ? __krow__::screen.height() : rect.h;
   int b = t + h - 1;
   if (w < 2 || h < 2) return;
+
   auto& s = *border_type_;
   bool has_top =
       (edges_ & krow::style::Borders::TOP) != krow::style::Borders::NONE;
@@ -24,26 +24,44 @@ void Block::draw() {
       (edges_ & krow::style::Borders::LEFT) != krow::style::Borders::NONE;
   bool has_right =
       (edges_ & krow::style::Borders::RIGHT) != krow::style::Borders::NONE;
+
   for (int x = l; x <= r; ++x) {
     if (has_top) __krow__::drawObj.put(t, x, {s.h, border_style_});
     if (has_bottom) __krow__::drawObj.put(b, x, {s.h, border_style_});
   }
+  for (int y = t; y <= b; ++y) {
+    if (has_left) __krow__::drawObj.put(y, l, {s.v, border_style_});
+    if (has_right) __krow__::drawObj.put(y, r, {s.v, border_style_});
+  }
+  if (has_top && has_left) __krow__::drawObj.put(t, l, {s.tl, border_style_});
+  if (has_top && has_right) __krow__::drawObj.put(t, r, {s.tr, border_style_});
+  if (has_bottom && has_left)
+    __krow__::drawObj.put(b, l, {s.bl, border_style_});
+  if (has_bottom && has_right)
+    __krow__::drawObj.put(b, r, {s.br, border_style_});
 
   if (!title_.empty()) {
     for (size_t i = 0; i < title_.contents_.size(); ++i) {
       const auto& row = title_.contents_[i];
-
-      auto draw_alignment = [&](const std::queue<Line>& q,
-                                style::alignment align) {
+      auto draw_alignment = [&](std::queue<Line> q, style::alignment align,
+                                const std::string& align_label) {
         if (q.empty()) return;
+        Line merged_line;
+        while (!q.empty()) {
+          Line front_line = q.front();
+          q.pop();
+          for (const auto& item : front_line.contents_) {
+            merged_line.contents_.push_back(item);
+          }
+        }
 
-        Line line = q.front();
         int title_len = 0;
-        for (const auto& c : line.contents_) {
+        for (const auto& c : merged_line.contents_) {
           title_len += __krow__::get_visual_width(c.first.text_);
         }
+
         int start_ = __krow__::calc_alignment(align, {l + 1, r - 1}, title_len);
-        for (const auto& l : line.contents_) {
+        for (const auto& l : merged_line.contents_) {
           auto v_chars = __krow__::split_by_visual_character(l.first.text_);
 
           for (const auto& vc : v_chars) {
@@ -54,25 +72,11 @@ void Block::draw() {
         }
       };
 
-      draw_alignment(row.left, style::alignment::LEFT);
-      draw_alignment(row.center, style::alignment::CENTER);
-      draw_alignment(row.right, style::alignment::RIGHT);
+      draw_alignment(row.left, style::alignment::LEFT, "LEFT");
+      draw_alignment(row.center, style::alignment::CENTER, "CENTER");
+      draw_alignment(row.right, style::alignment::RIGHT, "RIGHT");
     }
   }
-
-  for (int y = t; y <= b; ++y) {
-    if (has_left) __krow__::drawObj.put(y, l, {s.v, border_style_});
-    if (has_right) __krow__::drawObj.put(y, r, {s.v, border_style_});
-  }
-  if (has_top && has_left) __krow__::drawObj.put(t, l, {s.tl, border_style_});
-
-  if (has_top && has_right) __krow__::drawObj.put(t, r, {s.tr, border_style_});
-
-  if (has_bottom && has_left)
-    __krow__::drawObj.put(b, l, {s.bl, border_style_});
-
-  if (has_bottom && has_right)
-    __krow__::drawObj.put(b, r, {s.br, border_style_});
 }
 
 }  // namespace krow
