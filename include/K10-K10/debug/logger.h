9c43fb52e56f55569259d2437c21cd282399bebb
K10-K10 <keito.tottori@gmail.com>
2026-06-19 05:06:52 +0000

    feat: add new logger type

--- include/K10-K10/debug/logger.h
+++ include/K10-K10/debug/logger.h
@@ -2,6 +2,7 @@
 #define INCLUDE_DEBUG_LOGGER_H_
 
 #include <cstdint>
+#include <fstream>
 #include <mutex>
 #include <string>
 #include <vector>
@@ -11,7 +12,7 @@ namespace __krow__ {
 #ifdef ENABLE_LOGGER
 class logger {
  public:
-  enum class log_type : std::uint8_t { success, failed, error };
+  enum class log_type : std::uint8_t { success, failed, error, info };
   struct log_value {
     unsigned int id;
     log_type type;
@@ -23,9 +24,34 @@ class logger {
 
   logger() = delete;
 
+  static void set_log_file(const std::string& filename) {
+    std::lock_guard<std::mutex> lock(mtx);
+    log_file_path = filename;
+  }
+
   static void add_log(log_type type_, const std::string& message) {
     std::lock_guard<std::mutex> lock(mtx);
     logs.push_back({id++, type_, message});
+
+    std::ofstream ofs(log_file_path, std::ios::app);
+    if (ofs.is_open()) {
+      ofs << "[" << (id - 1) << "] ";
+      switch (type_) {
+        case log_type::success:
+          ofs << "[SUCCESS] ";
+          break;
+        case log_type::failed:
+          ofs << "[FAILED]  ";
+          break;
+        case log_type::error:
+          ofs << "[ERROR]   ";
+          break;
+        case log_type::info:
+          ofs << "[INFO]   ";
+          break;
+      }
+      ofs << message << "\n";
+    }
   }
 
   static std::vector<log_value> get_logs() {
@@ -37,18 +63,21 @@ class logger {
     std::lock_guard<std::mutex> lock(mtx);
     logs.clear();
     id = 1;
+    std::ofstream ofs(log_file_path, std::ios::trunc);
+    ofs.close();
   }
 
  private:
   inline static unsigned int id = 1;
   inline static std::vector<log_value> logs;
   inline static std::mutex mtx;
+  inline static std::string log_file_path = "debug.log";
 };
 
 #else
 class logger {
  public:
-  enum class log_type : std::uint8_t { success, failed, error };
+  enum class log_type : std::uint8_t { success, failed, error, info };
   struct log_value {
     unsigned int id;
     log_type type;
@@ -58,10 +87,9 @@ class logger {
 
   logger() = delete;
 
+  static void set_log_file(const std::string& filename) {}
   static void add_log(log_type type_, const std::string& message) {}
-
   static std::vector<log_value> get_logs() { return {}; }
-
   static void clear_logs() {}
 };
 
